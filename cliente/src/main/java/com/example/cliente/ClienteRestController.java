d6bc4537e45bcf4b1425ee50f75637d41e8cabf2
Diego Lirio <diegolirio.dl@gmail.com>
2020-05-05 11:17:16 -0300

    Timeout HTTP Client

--- cliente/src/main/java/com/example/cliente/ClienteRestController.java
+++ cliente/src/main/java/com/example/cliente/ClienteRestController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteRestController {
 
     @GetMapping("/{id}")
-    public Cliente getById(@PathVariable Long id) {
+    public Cliente getById(@PathVariable Long id) throws InterruptedException {
+        //TimeUnit.SECONDS.sleep(30L);
         return new Cliente(id, UUID.randomUUID().toString());
     }
 
