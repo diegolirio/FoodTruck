53c33698fdcbe8b7529876c641321485e05894b3
Diego Lirio <diegolirio.dl@gmail.com>
2020-09-05 01:20:12 -0300

    ContextStack exclude

--- cliente/src/main/java/com/example/cliente/ClienteApplication.java
+++ cliente/src/main/java/com/example/cliente/ClienteApplication.java
@@ -3,13 +3,14 @@ package com.example.cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.example.logtracing", "com.example.cliente"})
 public class ClienteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClienteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
 
 }
